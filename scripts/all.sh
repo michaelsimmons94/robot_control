d1b46be97b9d6e9b30c244f24f949d62d081004c
Jordan Henstrom <jhenstrom@gmail.com>
2019-06-03 14:13:56 -0600

    Update all.sh
    Made it start everything it needed to for robot control

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,35 +1,21 @@
 #!/usr/bin/env bash
 
-#echo "Start Joint Tragectory Service"
-#rosrun intera_interface joint_trajectory_action_server.py &
+echo "Start Joint Tragectory Service"
+rosrun intera_interface joint_trajectory_action_server.py &
 
-#echo "Start gripper server"
-#rosrun robot_control open_close_gripper_server.py &
+echo "Start gripper server"
+rosrun robot_control open_close_gripper_server.py &
 
-#cd ../../sawyer_moveit
+cd ../../sawyer_moveit
 
-#./sawyer_clicksmart_moveit_launch.sh &
+./sawyer_clicksmart_moveit_launch.sh &
 # Start Custom Services
-
-
-echo "Start Service: get_xyz"
-rosrun robot_control get_xyz_service.py joint_states:=/robot/joint_states &
 
 echo "Start Service: goto_xyz"
 rosrun robot_control goto_xyz_service.py joint_states:=/robot/joint_states &
-
-echo "Start Service get_gripper_status_service"
-rosrun robot_control get_gripper_status_service.py &
 
 echo "Start Service: manipulate_head_service"
 rosrun robot_control get_gripper_status_service.py &
 
 echo "Start Service: move_arm_out_of_way_service"
 rosrun robot_control move_arm_out_of_way_service.py &
-
-echo "Start Service: actuate_gripper_service"
-rosrun robot_control actuate_gripper_service.py &
-
-echo "Start Service: get_robot_is_moving_service"
-rosrun robot_control get_robot_is_moving_service.py &
-
